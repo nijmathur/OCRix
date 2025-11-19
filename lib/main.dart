4a1e3e5229c5333ac3bf0c44225d0820ed974f2f
Nij Mathur <nijanand.m@gmail.com>
2025-11-18 21:37:59 -0500

    fix: Use CardThemeData instead of CardTheme for Flutter 3.35.7 compatibility

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class OCRixApp extends ConsumerWidget {
           centerTitle: true,
           elevation: 0,
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
@@ -64,7 +64,7 @@ class OCRixApp extends ConsumerWidget {
           centerTitle: true,
           elevation: 0,
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemeData(
           elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
